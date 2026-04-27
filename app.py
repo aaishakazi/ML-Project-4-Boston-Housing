fc4fd517b7419232185da1779575c1a7b45e4695
aaishakazi <aaishakazi0808@gmail.com>
2026-04-27 16:13:11 +0300

    App update

---
+++ app.py
@@ -0,0 +1,56 @@
+import pickle
+from flask import Flask, request, jsonify, app, url_for, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+model = pickle.load(open('housing_model.pkl', 'rb'))
+features = pickle.load(open('model_features.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+
+        data = request.get_json(force=True)
+
+        raw_features = ['MedInc', 'HouseAge', 'AveRooms', 'AveBedrms', 'Population', 'AveOccup', 'Latitude', 'Longitude']
+        
+        try:
+            input_data = {f: [data[f]] for f in raw_features}
+        except KeyError as e:
+            return jsonify({'error': f'Missing input field: {str(e)}'}), 400
+        
+        input_df = pd.DataFrame(input_data)
+        
+        # Feature engineering for the new data 
+        input_df['IncPerOcc'] = input_df['MedInc'] / input_df['AveOccup']
+        input_df['RoomsPerHousehold'] = input_df['AveRooms'] / input_df['AveOccup']
+        input_df['BedroomsPerRoom'] = input_df['AveBedrms'] / input_df['AveRooms']
+
+        hub_lat, hub_lon = 37.7749, -122.4194
+        input_df['Distance_to_Nearest_Hub'] = np.sqrt(
+            (input_df['Latitude'] - hub_lat)**2 + (input_df['Longitude'] - hub_lon)**2
+        )
+        
+        input_df['Wealth_Location_Score'] = input_df['MedInc'] / (input_df['Distance_to_Nearest_Hub'] + 1)
+        
+        # Align Columns
+        input_df = input_df[features]
+        
+        prediction = model.predict(input_df)[0]
+
+        return jsonify({
+                'status': 'success',
+                'predicted_price_usd': round(float(prediction * 100000), 2),
+                'currency': 'USD'
+            })
+
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 400 
+
+if __name__ == '__main__':
+    app.run(debug=True)
